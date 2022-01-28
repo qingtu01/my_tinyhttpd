5c22b7c574d15b2a9f65600b72f4f7b83236e532
qingtu01 <763649367@qq.com>
2022-01-28 23:48:38 +0800

    添加port变量说明注释

--- httpd.c
+++ httpd.c
@@ -476,7 +476,9 @@ void unimplemented(int client)
 int main(void)
 {
  int server_sock = -1;
- u_short port = 0;
+ 
+ u_short port = 0;//系统自动分配端口
+
  int client_sock = -1;
  struct sockaddr_in client_name;
  int client_name_len = sizeof(client_name);
